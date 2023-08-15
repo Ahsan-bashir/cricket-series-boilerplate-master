3096f5971ab669aca1ea739a6c84f487c51e5fab
Ahsan-bashir <bteeccc@gmail.com>
2023-08-15 21:19:15 +0500

    project uploaded

---
+++ src/PerformanceCalc.js
@@ -0,0 +1,10 @@
+
+const PerformanceCalculator=(runs,balls)=>{
+     if(runs>30 && balls<15){
+        "1"
+     }
+     if(runs>50 && balls<20){"10"}
+     if(runs>100 && balls<50){"20"}
+}
+
+module.exports={PerformanceCalculator}
